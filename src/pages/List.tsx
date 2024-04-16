29cd27b3d7fc7a7095b37570adb968c74ee2af0d
olaknowct <olanochristopher@gmail.com>
2024-04-16 16:54:00 +0800

    add burger buttons that toggles side nav menu

--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 
 const List: React.FC = () => {
@@ -6,7 +6,11 @@ const List: React.FC = () => {
     return (
         <IonPage>
             <IonHeader>
-                <IonToolbar>
+                <IonToolbar color={'success'}>
+
+                    <IonButtons slot='start' >
+                        <IonMenuButton />
+                    </IonButtons>
                     <IonTitle>List</IonTitle>
                 </IonToolbar>
             </IonHeader>
