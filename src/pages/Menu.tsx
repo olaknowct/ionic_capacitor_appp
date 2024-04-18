b53d5f456689f6f1f944e84b6bb27a55e4d49a19
olaknowct <olanochristopher@gmail.com>
2024-04-18 15:39:13 +0800

    add logout side nav item

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,9 +1,9 @@
-import { IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonIcon, IonItem, IonMenu, IonMenuToggle, IonPage, IonRouterOutlet, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
 import { Redirect, Route } from 'react-router';
 import List from './List';
 import Settings from './Settings';
-import { homeOutline, newspaperOutline } from 'ionicons/icons';
+import { homeOutline, logOutOutline, newspaperOutline } from 'ionicons/icons';
 
 const Menu: React.FC = () => {
 
@@ -30,7 +30,14 @@ const Menu: React.FC = () => {
                                 </IonItem>
                             </IonMenuToggle>
                         ))}
+                        <IonMenuToggle autoHide={false}>
+                            <IonButton expand="full" routerLink="/" routerDirection="root">
+                                <IonIcon slot="start" icon={logOutOutline} />
+                                Logout
+                            </IonButton>
+                        </IonMenuToggle>
                     </IonContent>
+
                 </IonMenu>
                 <IonRouterOutlet id="main">
                     <Route exact path="/app/list" component={List} />
