9ba40b6fbb69953f2490792d350eefc5a676ab66
olaknowct <olanochristopher@gmail.com>
2024-04-24 11:33:13 +0800

    add tab 2 animation of ion button using create animation

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,34 @@
-
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import React from 'react';
+import { CreateAnimation, Gesture, GestureDetail, IonButton, IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar, createGesture, useIonViewDidEnter } from '@ionic/react';
+import React, { useRef } from 'react';
 
 const Tab2: React.FC = () => {
+    const animationRef = useRef<CreateAnimation | null>(null);
 
     return (
         <IonPage>
             <IonHeader>
                 <IonToolbar color={'success'}>
-
-                    <IonButtons slot='start' >
+                    <IonButtons slot="start">
                         <IonMenuButton />
                     </IonButtons>
                     <IonTitle>Tab 2</IonTitle>
                 </IonToolbar>
             </IonHeader>
-            <IonContent className="ion-padding">
-                UI goes here...
+            <IonContent className="ion-padding" scrollY={false}>
+                <CreateAnimation
+                    ref={animationRef}
+                    duration={2000}
+                    iterations={Infinity}
+                    delay={1000}
+                    keyframes={[
+                        { offset: 0, transform: 'scale(1)', opacity: '1' },
+                        { offset: 0.5, transform: 'scale(1.5)', opacity: '0.5' },
+                        { offset: 1, transform: 'scale(1)', opacity: '1' },
+                    ]}>
+                    <IonButton expand="block" color={'success'} className="ion-margin">
+                        Join Ionic Academy
+                    </IonButton>
+                </CreateAnimation>
             </IonContent>
         </IonPage>
     );
